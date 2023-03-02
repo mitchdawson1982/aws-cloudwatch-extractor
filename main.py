86e7653f570a83e2f81a187ed541a473f749fdea
Mitch Dawson <mitchell.dawson@justice.gov.uk>
2023-03-02 16:01:59 +0000

    :construction: Made the inputs more generic

--- main.py
+++ main.py
@@ -5,11 +5,9 @@ from datetime import timedelta
 
 from log_extractor import LogExtractor
 
-REGION_NAME = "eu-west-2"
-VPC_LOG_GROUP_NAME = "staff-device-dns-dhcp-production-vpc-flow-logs-log-group"
-VPC_FILTER_STRING = "?10.180.80.5 ?10.180.81.5" # -"10.180.80" "ACCEPT OK"
-PRIMARY_FILTER_LIST = ["10.180.80.5", "10.180.81.5"]
-DNS_FILTER_STRING = ""
+REGION = ""
+LOG_GROUP_NAME = ""
+SEARCH_STRING = ""
 CLIENT_TYPE = "logs"
 LIMIT = 10000
 
@@ -20,17 +18,14 @@ end_time =dt.strptime(endTimeHuman, '%Y-%m-%dT%H:%M:%S.%f+01:00')
 start_timestamp = int(start_time.timestamp()) * 1000
 end_timestamp = int(end_time.timestamp()) * 1000
 
-print(start_timestamp, end_timestamp)
-
-
 
 def main():
        
     
     vpc = LogExtractor(
-    region_name=REGION_NAME, log_group_name=VPC_LOG_GROUP_NAME,
-    start_time=start_timestamp, end_time=end_timestamp, filter_list=PRIMARY_FILTER_LIST,
-    limit=LIMIT, search_string=VPC_FILTER_STRING, client_type=CLIENT_TYPE
+    region_name=REGION, log_group_name=LOG_GROUP_NAME,
+    start_time=start_timestamp, end_time=end_timestamp,
+    limit=LIMIT, search_string=SEARCH_STRING, client_type=CLIENT_TYPE
     )
     vpc.run()
     print(len(vpc.events))
